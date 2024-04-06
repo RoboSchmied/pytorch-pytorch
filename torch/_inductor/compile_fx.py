f96928d5c65c32115869e0119c21f391c9ec5ba4
Shunting Zhang <shunting@fb.com>
2024-04-06 10:28:09 -0700

    [WIP][inductor] padding
    ghstack-source-id: b2784800f3a9cf4a744b2d8845ce5625888a7e8e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120758

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -315,6 +315,22 @@ def is_tf32_warning_applicable(gm: torch.fx.GraphModule):
     return False
 
 
+def maybe_disable_comprehensive_padding(example_inputs: List[torch.Tensor]):
+    """
+    TODO: for CPU backend, enable comprehensive padding causes some unit tests
+    fail due to changing number of generated kernels. Skip for now.
+    """
+    all_cpu = not any(
+        t.device.type == "cuda" for t in example_inputs if isinstance(t, torch.Tensor)
+    )
+
+    if config.comprehensive_padding and all_cpu:
+        perf_hint_log.warning("Skip comprehensive padding on CPU")
+        return config.patch(comprehensive_padding=False)
+    else:
+        return contextlib.nullcontext()
+
+
 @DebugContext.wrap
 def count_bytes_inner(
     gm: torch.fx.GraphModule,
@@ -329,7 +345,9 @@ def count_bytes_inner(
         _recursive_post_grad_passes(gm, False)
 
     graph = GraphLowering(gm, shape_env=shape_env, num_static_inputs=num_fixed)
-    with V.set_graph_handler(graph), V.set_real_inputs(example_inputs):
+    with V.set_graph_handler(graph), V.set_real_inputs(
+        example_inputs
+    ), maybe_disable_comprehensive_padding(example_inputs):
         graph.run(*example_inputs)
         num_bytes, nodes_num_elem, node_runtimes = graph.count_bytes()
         metrics.num_bytes_accessed += num_bytes
@@ -383,6 +401,7 @@ def get_patched_config_dict(config_patches=None) -> Dict[str, Any]:
 # compile_fx return and we may want to use the _LazyGraphModule for compiling
 # the backward graph as well.
 @_use_lazy_graph_module(dynamo_config.use_lazy_graph_module)
+# @skip_grad_layout_contract()
 @dynamo_utils.dynamo_timed(phase_name="inductor_compile")
 def compile_fx_inner(
     gm: torch.fx.GraphModule,
@@ -676,7 +695,9 @@ def fx_codegen_and_compile(
         )
         log.debug("optimus parameter sent to the scuba: %s", optimus_scuba_log)
 
-    with V.set_fake_mode(fake_mode):
+    with V.set_fake_mode(fake_mode), maybe_disable_comprehensive_padding(
+        example_inputs
+    ):
         const_output_index = None
         const_graph = None
         const_code = None
@@ -1147,6 +1168,7 @@ def fw_compiler_freezing(
 
 
 @_use_lazy_graph_module(dynamo_config.use_lazy_graph_module)
+# @skip_grad_layout_contract()
 def compile_fx(
     model_: torch.fx.GraphModule,
     example_inputs_: List[torch.Tensor],
@@ -1155,6 +1177,11 @@ def compile_fx(
     decompositions: Optional[Dict[OpOverload, Callable[..., Any]]] = None,
 ):
     """Main entrypoint to a compile given FX graph"""
+    if config.comprehensive_padding and config.skip_grad_layout_contract:
+        torch._C._autograd._set_skip_grad_layout_contract(True)
+        print("skip_grad_layout_contract")
+    else:
+        print("no skip_grad_layout_contract")
     if config_patches:
         with config.patch(config_patches):
             return compile_fx(
@@ -1339,8 +1366,20 @@ def compile_fx(
         )
 
     @dynamo_utils.dynamo_timed
-    @dynamo_utils.maybe_cprofile
+    # @dynamo_utils.maybe_cprofile
     def bw_compiler(model: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
+        user_visible_outputs = set()
+
+        if config.bw_outputs_user_visible:
+            *_, model_outputs_node = model.graph.nodes
+            assert model_outputs_node.op == "output"
+            model_outputs = pytree.arg_tree_leaves(*model_outputs_node.args)
+            user_visible_outputs = {
+                n.name
+                for n in model_outputs
+                if isinstance(n, torch.fx.Node)
+            }
+            # print(f"BWD User visiable: {user_visible_outputs}") # TODO
         fixed = count_tangents(model)
         return inner_compile(
             model,
@@ -1350,6 +1389,7 @@ def compile_fx(
             is_backward=True,
             graph_id=graph_id,
             boxed_forward_device_index=forward_device,
+            user_visible_outputs=user_visible_outputs,
         )
 
     # TODO: can add logging before/after the call to create_aot_dispatcher_function
